62dec9e7d9bf912ca2db6761e6c7816a68fba0d0
Jason Hayhoe <77281904+jazonh18@users.noreply.github.com>
2024-04-12 19:49:54 -0400

    Delete hdmi_sw directory
    Deleting redundant files for HDMI/GUI

--- hdmi_sw/image_load.tcl
+++
@@ -1,8 +0,0 @@
-mwr -bin -file ECE532_trumpet_asset_full_note_7.bin 0x91000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_6.bin 0x92000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_5.bin 0x93000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_4.bin 0x94000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_3.bin 0x95000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_2.bin 0x96000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_1.bin 0x97000000 1440800
-mwr -bin -file ECE532_trumpet_asset_full_note_0.bin 0x98000000 1440800
